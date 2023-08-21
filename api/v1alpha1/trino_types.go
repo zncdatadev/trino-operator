781640f4d75e95fb4de1148194cc83c23375deb4
whg517 <huagang517@126.com>
2023-08-21 14:37:28 +0800

    feat: add api and reconciler

--- api/v1alpha1/trino_types.go
+++ api/v1alpha1/trino_types.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -25,17 +26,107 @@ import (
 
 // TrinoSpec defines the desired state of Trino
 type TrinoSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	Image ImageSpec `json:"image,omitempty"`
 
-	// Foo is an example field of Trino. Edit trino_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// +kubebuilder:validation=Optional
+	// +kubebuilder:default=1
+	Replicas int32 `json:"replicas,omitempty"`
+
+	Resource *corev1.ResourceRequirements `json:"resource,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	SecurityContext *corev1.SecurityContext `json:"securityContext,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	PodSecurityContext *corev1.PodSecurityContext `json:"podSecurityContext,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	Service *ServiceSpec `json:"service,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	NodeSelector map[string]string `json:"nodeSelector,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	Tolerations []corev1.Toleration `json:"tolerations,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	Persistence *PersistenceSpec `json:"persistence,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	Ingress *IngressSpec `json:"ingress,omitempty"`
+}
+
+func (instance *Trino) GetNameWithSuffix(name string) string {
+	return instance.GetName() + "-" + name
+}
+
+type ImageSpec struct {
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=trinodb/trino
+	Repository string `json:"repository,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=latest
+	Tag string `json:"tag,omitempty"`
+
+	// +kubebuilder:validation:Enum=Always;Never;IfNotPresent
+	// +kubebuilder:default=IfNotPresent
+	PullPolicy corev1.PullPolicy `json:"pullPolicy,omitempty"`
+}
+
+type ServiceSpec struct {
+	// +kubebuilder:validation:Optional
+	Annotations map[string]string `json:"annotations,omitempty"`
+	// +kubebuilder:validation:enum=ClusterIP;NodePort;LoadBalancer;ExternalName
+	// +kubebuilder:default=ClusterIP
+	Type corev1.ServiceType `json:"type,omitempty"`
+
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:validation:Maximum=65535
+	// +kubebuilder:default=18080
+	Port int32 `json:"port,omitempty"`
+}
+
+type IngressSpec struct {
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=spark-history.example.com
+	Host string `json:"host,omitempty"`
+}
+
+type PersistenceSpec struct {
+	// +kubebuilder:validation:Optional
+	StorageClass *string `json:"storageClass,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default={ReadWriteOnce}
+	AccessModes []corev1.PersistentVolumeAccessMode `json:"accessModes,omitempty"`
+
+	// +kubebuilder:default="10Gi"
+	Size string `json:"size,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	ExistingClaim *string `json:"existingClaim,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=Filesystem
+	VolumeMode *corev1.PersistentVolumeMode `json:"volumeMode,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	Labels map[string]string `json:"labels,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	Annotations map[string]string `json:"annotations,omitempty"`
+}
+
+// GetPvcName returns the name of the PVC for the HiveMetastore.
+func (instance *Trino) GetPvcName() string {
+	return instance.GetNameWithSuffix("pvc")
 }
 
 // TrinoStatus defines the observed state of Trino
 type TrinoStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	Nodes      []string                    `json:"nodes"`
+	Conditions []corev1.ComponentCondition `json:"conditions"`
 }
 
 //+kubebuilder:object:root=true
