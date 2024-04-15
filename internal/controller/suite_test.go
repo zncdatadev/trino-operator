8c303d19ddbe3802333b050f618be113be7a714e
whg517 <huagang517@126.com>
2024-04-15 11:37:43 +0800

    chore: add test config (#44)

--- internal/controller/suite_test.go
+++ internal/controller/suite_test.go
@@ -17,7 +17,11 @@ limitations under the License.
 package controller
 
 import (
+	"context"
+	"fmt"
+	"os"
 	"path/filepath"
+	"runtime"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -25,6 +29,7 @@ import (
 
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
@@ -37,11 +42,23 @@ import (
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
 // http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
 
-var cfg *rest.Config
-var k8sClient client.Client
-var testEnv *envtest.Environment
+var (
+	cfg            *rest.Config
+	k8sClient      client.Client
+	testEnv        *envtest.Environment
+	ctx            context.Context
+	cancel         context.CancelFunc
+	testK8sVersion string
+)
 
 func TestAPIs(t *testing.T) {
+	testK8sVersion = os.Getenv("ENVTEST_K8S_VERSION")
+
+	if testK8sVersion == "" {
+		logf.Log.Info("ENVTEST_K8S_VERSION not set, using default version 1.26.1")
+		testK8sVersion = "1.26.1"
+	}
+
 	RegisterFailHandler(Fail)
 
 	RunSpecs(t, "Controller Suite")
@@ -49,8 +66,14 @@ func TestAPIs(t *testing.T) {
 
 var _ = BeforeSuite(func() {
 	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
-
+	ctx, cancel = context.WithCancel(context.TODO())
 	By("bootstrapping test environment")
+
+	Expect(os.Setenv(
+		"KUBEBUILDER_ASSETS",
+		fmt.Sprintf("../../bin/k8s/%s-%s-%s", testK8sVersion, runtime.GOOS, runtime.GOARCH),
+	)).To(Succeed())
+
 	testEnv = &envtest.Environment{
 		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "config", "crd", "bases")},
 		ErrorIfCRDPathMissing: true,
@@ -71,10 +94,27 @@ var _ = BeforeSuite(func() {
 	Expect(err).NotTo(HaveOccurred())
 	Expect(k8sClient).NotTo(BeNil())
 
+	k8sManager, err := ctrl.NewManager(cfg, ctrl.Options{
+		Scheme: scheme.Scheme,
+	})
+	Expect(err).ToNot(HaveOccurred())
+
+	err = (&TrinoReconciler{
+		Client: k8sManager.GetClient(),
+		Scheme: k8sManager.GetScheme(),
+	}).SetupWithManager(k8sManager)
+	Expect(err).ToNot(HaveOccurred())
+
+	//go func() {
+	//	defer GinkgoRecover()
+	//	err = k8sManager.Start(ctx)
+	//	Expect(err).ToNot(HaveOccurred(), "failed to run manager")
+	//}()
 })
 
 var _ = AfterSuite(func() {
+	cancel()
 	By("tearing down the test environment")
-	err := testEnv.Stop()
-	Expect(err).NotTo(HaveOccurred())
+	Expect(testEnv.Stop()).To(Succeed())
+	Expect(os.Unsetenv("KUBEBUILDER_ASSETS")).To(Succeed())
 })
